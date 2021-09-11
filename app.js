61c962accdc248f8f63f56de3f69524254a03e3d
AnnaKireyeva <anna.kireyeva14@gmail.com>
2021-09-12 00:50:48 +0300

    без ролистывания изображений галереи в открытом модальном окне

---
+++ app.js
@@ -0,0 +1,146 @@
+ const galleryItems = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
+
+const galleryList = document.querySelector('.js-gallery');
+
+const modalLightbox = document.querySelector('.js-lightbox');
+
+const modalImage = document.querySelector('.lightbox__image');
+
+const modalCloseBtn = document.querySelector('.lightbox__button');
+
+const overlay = document.querySelector('.lightbox__overlay');
+
+function createGallaryMarkup(galleryItems) {
+  return galleryItems.map(({preview, original, description}) => {
+    return `
+    <li class="gallery__item">
+      <a
+        class="gallery__link"
+        href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`;
+  }).join('');
+  
+}
+// console.log(createGallaryMarkup(galleryItems));
+
+const galleryMarkup = createGallaryMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+galleryList.addEventListener('click', onGalleryElemClickOpenModal);
+
+function onGalleryElemClickOpenModal(evt) {
+  evt.preventDefault();
+  
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const bigImageLink = evt.target.dataset.source;
+  console.log(bigImageLink);
+ 
+  // const modalLightbox = document.querySelector('.js-lightbox');
+  
+  modalLightbox.classList.add('is-open');
+  window.addEventListener('keydown', onEscKeyPressCloseModal);
+
+  // const modalImage = document.querySelector('.lightbox__image');
+  
+  modalImage.src = bigImageLink;
+  modalImage.alt = evt.target.alt;
+}
+
+modalCloseBtn.addEventListener('click', onModalClose);
+  
+function onModalClose(evt) {
+  modalLightbox.classList.remove('is-open');
+  modalImage.src = "";
+  modalImage.alt = "";
+  window.removeEventListener('keydown', onEscKeyPressCloseModal);
+}
+
+overlay.addEventListener('click', onOverlayClickClose);
+
+function onOverlayClickClose(evt) {
+  if (evt.currentTarget === evt.target) {
+    onModalClose();
+  }
+}
+
+function onEscKeyPressCloseModal(evt) {
+  const esc_key_code = 'Escape';
+    if(evt.code === esc_key_code) {
+        onModalClose();
+        }
+}
